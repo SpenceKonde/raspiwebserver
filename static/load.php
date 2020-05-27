9e9b96908a19828fa635ae3e63288f24ffa4fc00
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2020-05-27 17:58:47 -0400

    Update load.php

--- static/load.php
+++ static/load.php
@@ -1,10 +1,10 @@
 <?php 
 if ($_GET['index'] > -1 && $_GET['index']<101) {
-file_get_contents("http://pong1/load.cmd?" . "index=".$_GET['index']);
-file_get_contents("http://pong2/load.cmd?" . "index=".$_GET['index']);
-file_get_contents("http://pong3/load.cmd?" . "index=".$_GET['index']);
-file_get_contents("http://pong4/load.cmd?" . "index=".$_GET['index']);
-file_get_contents("http://pong5/load.cmd?" . "index=".$_GET['index']);
-file_get_contents("http://pong6/load.cmd?" . "index=".$_GET['index']);
-file_get_contents("http://pong7/load.cmd?" . "index=".$_GET['index']);
+file_get_contents("http://192.168.2.60/load.cmd?" . "index=".$_GET['index']);
+file_get_contents("http://192.168.2.61/load.cmd?" . "index=".$_GET['index']);
+file_get_contents("http://192.168.2.62/load.cmd?" . "index=".$_GET['index']);
+file_get_contents("http://192.168.2.63/load.cmd?" . "index=".$_GET['index']);
+file_get_contents("http://192.168.2.64/load.cmd?" . "index=".$_GET['index']);
+file_get_contents("http://192.168.2.65/load.cmd?" . "index=".$_GET['index']);
+file_get_contents("http://192.168.2.66/load.cmd?" . "index=".$_GET['index']);
 }
